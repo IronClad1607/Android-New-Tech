d71740d4b65346e6ab9447bc811f6e904a5355ab
Ishaan Jain <ishaan160799@gmail.com>
2020-10-03 22:13:39 +0530

    data binding: kotlin started

--- Data Binding Demo/app/src/main/java/com/ironclad/myapplication/MainActivity.kt
+++ Data Binding Demo/app/src/main/java/com/ironclad/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ironclad.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnSubmit)
+        button.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayGreeting() {
+        val messageView = findViewById<TextView>(R.id.tvGreetings)
+        val nameText = findViewById<EditText>(R.id.etName)
+
+        messageView.text = "Hello! ${etName.text}"
     }
 }
