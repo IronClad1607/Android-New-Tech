7f5bd3a81312b67a07a9ceaba6d41f692f14c516
Ishaan Jain <ishaan160799@gmail.com>
2020-10-03 23:38:25 +0530

    viewmodel:data binding implemented

--- ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivity.kt
+++ ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.ironclad.viewmodeldemo
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ironclad.viewmodeldemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tvCount.text = count.toString()
+
+        binding.btnClick.setOnClickListener {
+            count++
+            binding.tvCount.text = count.toString()
+        }
     }
 }
