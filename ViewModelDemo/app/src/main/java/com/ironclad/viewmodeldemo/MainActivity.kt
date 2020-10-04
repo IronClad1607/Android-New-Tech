561141087b35f1fcceb134174c946f189f5d8705
Ishaan Jain <ishaan160799@gmail.com>
2020-10-04 13:33:28 +0530

    viewmodel: viewmodel implemented

--- ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivity.kt
+++ ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivity.kt
@@ -4,20 +4,22 @@ import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.ironclad.viewmodeldemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var count = 0;
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.tvCount.text = count.toString()
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.tvCount.text = viewModel.count.toString()
 
         binding.btnClick.setOnClickListener {
-            count++
-            binding.tvCount.text = count.toString()
+            binding.tvCount.text = viewModel.getUpdatedCount().toString()
         }
     }
 }
--- ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivityViewModel.kt
+++ ViewModelDemo/app/src/main/java/com/ironclad/viewmodeldemo/MainActivityViewModel.kt
@@ -3,4 +3,9 @@ package com.ironclad.viewmodeldemo
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+    var count = 0
+
+    fun getUpdatedCount(): Int {
+        return ++count
+    }
 }
