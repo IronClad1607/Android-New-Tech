b7026058d8c7326e79bf20bbeefa93401cff73fb
Ishaan Jain <ishaan160799@gmail.com>
2020-10-04 22:08:17 +0530

    two way: two way binding done

--- TwoWayDataBinding/app/src/main/java/com/ironclad/twowaydatabinding/MainActivityViewModel.kt
+++ TwoWayDataBinding/app/src/main/java/com/ironclad/twowaydatabinding/MainActivityViewModel.kt
@@ -1,13 +1,24 @@
 package com.ironclad.twowaydatabinding
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel : ViewModel(),Observable {
 
+    @Bindable
     val userName = MutableLiveData<String>()
 
     init {
         userName.value = "IronClad"
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
     }
 }
