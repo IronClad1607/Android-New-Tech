11e6c6e4b2b23d91a351ca08afb4f726c904669b
Ishaan Jain <ishaan160799@gmail.com>
2020-10-06 14:13:10 +0530

    navcc: email fragment

---
+++ NavCC/app/src/main/java/com/ironclad/navcc/EmailFragment.kt
@@ -0,0 +1,22 @@
+package com.ironclad.navcc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ironclad.navcc.databinding.FragmentEmailBinding
+
+class EmailFragment : Fragment() {
+    private lateinit var binding: FragmentEmailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_email, container, false)
+
+        return binding.root
+    }
+}
