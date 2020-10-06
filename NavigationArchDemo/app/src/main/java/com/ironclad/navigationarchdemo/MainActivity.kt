7a473faf5f1d48f2bcb22d34663e67f6824498d1
Ishaan Jain <ishaan160799@gmail.com>
2020-10-06 08:07:06 +0530

    nav:data binding added

--- NavigationArchDemo/app/src/main/java/com/ironclad/navigationarchdemo/MainActivity.kt
+++ NavigationArchDemo/app/src/main/java/com/ironclad/navigationarchdemo/MainActivity.kt
@@ -2,8 +2,14 @@ package com.ironclad.navigationarchdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ironclad.navigationarchdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
