e91523b2c9e3d6e09d1689ae8f540b159a42a051
Ishaan Jain <ishaan160799@gmail.com>
2020-10-07 13:05:48 +0530

    aa:thread switching done

--- AsyncAndAwaitDemo/app/src/main/java/com/ironclad/asyncandawaitdemo/MainActivity.kt
+++ AsyncAndAwaitDemo/app/src/main/java/com/ironclad/asyncandawaitdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ironclad.asyncandawaitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.IO).launch {
+        CoroutineScope(Dispatchers.Main).launch {
             Log.d("PUI", "Calculation Started!!")
-            val stock1 = async { getStock1() }
-            val stock2 = async { getStock2() }
+            val stock1 = async(Dispatchers.IO) {
+                getStock1()
+            }
+
+            val stock2 = async(Dispatchers.IO) {
+                getStock2()
+            }
             val total = stock1.await() + stock2.await()
-            Log.d("PUI", "Total: $total")
+
+            Toast.makeText(applicationContext, "Total:$total", Toast.LENGTH_SHORT).show()
         }
     }
 
