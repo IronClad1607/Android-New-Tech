e4b9fe42a23c44915e93b8b3c4a4b57eb1f100c2
Ishaan Jain <ishaan160799@gmail.com>
2020-10-08 21:59:13 +0530

    live data builder used

--- ViewModelScopeDemo/app/src/main/java/com/ironclad/viewmodelscopedemo/MainActivityViewModel.kt
+++ ViewModelScopeDemo/app/src/main/java/com/ironclad/viewmodelscopedemo/MainActivityViewModel.kt
@@ -1,22 +1,24 @@
 package com.ironclad.viewmodelscopedemo
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.ironclad.viewmodelscopedemo.model.User
+import androidx.lifecycle.liveData
 import com.ironclad.viewmodelscopedemo.repo.UserRepo
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivityViewModel : ViewModel() {
     private var userRepo = UserRepo()
-    var users = MutableLiveData<ArrayList<User>>()
-    fun getUserData() {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                users.postValue(userRepo.getUsers())
-            }
-        }
+//    var users = MutableLiveData<ArrayList<User>>()
+
+//    fun getUserData() {
+//        viewModelScope.launch {
+//            withContext(Dispatchers.IO) {
+//                users.postValue(userRepo.getUsers())
+//            }
+//        }
+//    }
+
+    var users = liveData(Dispatchers.IO) {
+        val result = userRepo.getUsers()
+        emit(result)
     }
 }
