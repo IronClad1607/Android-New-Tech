74e5d86092f2dfc6e0500d0dbd499de6a1bf5ac2
Ishaan Jain <ishaan160799@gmail.com>
2020-10-08 21:25:19 +0530

    lcs:dependency added

---
+++ LivecycleScopeDemo/app/src/main/java/com/ironclad/livecyclescopedemo/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.ironclad.livecyclescopedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ironclad.livecyclescopedemo.ui.main.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
