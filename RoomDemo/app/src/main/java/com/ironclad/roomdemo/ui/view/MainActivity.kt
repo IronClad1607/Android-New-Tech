1acb5576a76a13317560986ab5c95d0f4cf20b59
Ishaan Jain <ishaan160799@gmail.com>
2020-10-09 15:06:37 +0530

    room:view model complete and view done

--- RoomDemo/app/src/main/java/com/ironclad/roomdemo/ui/view/MainActivity.kt
+++ RoomDemo/app/src/main/java/com/ironclad/roomdemo/ui/view/MainActivity.kt
@@ -2,15 +2,38 @@ package com.ironclad.roomdemo.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ironclad.roomdemo.R
+import com.ironclad.roomdemo.data.db.SubscriberDatabase
+import com.ironclad.roomdemo.data.repo.SubscriberRepository
 import com.ironclad.roomdemo.databinding.ActivityMainBinding
+import com.ironclad.roomdemo.ui.viewmodels.SubscriberViewModel
+import com.ironclad.roomdemo.ui.viewmodels.SubscriberViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var subscriberViewModel: SubscriberViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val dao = SubscriberDatabase.getInstance(this).subscriberDao
+        val repo = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repo)
+        subscriberViewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
+
+        binding.mViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+
+        displaySubscribersList()
+    }
+
+    private fun displaySubscribersList() {
+        subscriberViewModel.subscribers.observe(this, Observer {
+            Log.d("PUI", it.toString())
+        })
     }
 }
