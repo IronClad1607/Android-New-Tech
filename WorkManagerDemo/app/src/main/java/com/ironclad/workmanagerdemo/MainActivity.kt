4bea814546b109b1d4c9df73fc932c61b5c84830
Ishaan Jain <ishaan160799@gmail.com>
2020-10-16 12:36:38 +0530

    workmanager: onetimerequest created

--- WorkManagerDemo/app/src/main/java/com/ironclad/workmanagerdemo/MainActivity.kt
+++ WorkManagerDemo/app/src/main/java/com/ironclad/workmanagerdemo/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ironclad.workmanagerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import androidx.work.Worker
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+
+        WorkManager.getInstance(applicationContext)
+            .enqueue(oneTimeWorkRequest)
     }
 }
