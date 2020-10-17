a380ae9d4bd522edd3799c8e0a794d0fdc30d448
Ishaan Jain <ishaan160799@gmail.com>
2020-10-17 16:09:43 +0530

    singleton added

--- DaggerDemo/app/src/main/java/com/ironclad/daggerdemo/SmartPhoneComponent.kt
+++ DaggerDemo/app/src/main/java/com/ironclad/daggerdemo/SmartPhoneComponent.kt
@@ -1,8 +1,10 @@
 package com.ironclad.daggerdemo
 
 import dagger.Component
+import javax.inject.Singleton
 
 
+@Singleton
 @Component(modules = [MemoryCardModule::class, LIBatteryModule::class])
 interface SmartPhoneComponent {
 
